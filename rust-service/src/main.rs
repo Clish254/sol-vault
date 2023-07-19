0c9b311baae57e2d9b849ba86f5552017da5b1e8
Clish254 <clishilla@gmail.com>
2023-07-19 19:15:01 +0300

    fix: send interest instruction instead of sending interest directly to the vault token account

--- rust-service/src/main.rs
+++ rust-service/src/main.rs
@@ -1,6 +1,6 @@
 use std::borrow::Borrow;
 
-use anchor_lang::AccountDeserialize;
+use anchor_lang::{AccountDeserialize, InstructionData, ToAccountMetas};
 use solana_client::nonblocking::rpc_client::RpcClient;
 use solana_client::{
     rpc_config::{RpcAccountInfoConfig, RpcProgramAccountsConfig},
@@ -9,6 +9,7 @@ use solana_client::{
 
 use anchor_lang::prelude::AnchorDeserialize;
 use solana_account_decoder::UiAccountEncoding;
+use solana_program::instruction::Instruction;
 use solana_program::program_pack::Pack;
 
 use solana_sdk::{
@@ -18,8 +19,8 @@ use solana_sdk::{
     transaction::Transaction,
 };
 use spl_associated_token_account::get_associated_token_address;
-use spl_token::state::{Account as TokenAccount, Mint as MintAccount};
-use spl_token::{instruction::transfer, ID as TOKEN_PROGRAM_ID};
+use spl_token::state::Mint as MintAccount;
+use spl_token::ID as TOKEN_PROGRAM_ID;
 use vault::Vault;
 
 #[derive(Clone, Debug)]
@@ -94,9 +95,9 @@ async fn disburse_interest() -> Result<(), Box<dyn std::error::Error>> {
 
     // Iterate through each vault and process them
     for (vault_address, vault_account) in vaults {
-        let deserialized_data: VaultRecord =
+        let vault_data: VaultRecord =
             VaultRecord::try_deserialize_unchecked(&mut vault_account.data.borrow())?;
-        let mint = deserialized_data.mint();
+        let mint = vault_data.mint();
 
         // get vault token account where we'll send interest
         let (vault_token_account_pda, _bump) = Pubkey::find_program_address(
@@ -104,13 +105,10 @@ async fn disburse_interest() -> Result<(), Box<dyn std::error::Error>> {
             &vault_program_id,
         );
 
-        let serialized_token_account_data = rpc_client
-            .get_account_data(&vault_token_account_pda)
-            .await
-            .unwrap();
-
-        let token_account_data =
-            TokenAccount::unpack_from_slice(&serialized_token_account_data).unwrap();
+        let (vault_authority_pda, _bump) = Pubkey::find_program_address(
+            &[b"authority".as_ref(), vault_address.as_ref()],
+            &vault_program_id,
+        );
 
         let serialized_mint_account_data = rpc_client.get_account_data(mint).await.unwrap();
 
@@ -122,29 +120,42 @@ async fn disburse_interest() -> Result<(), Box<dyn std::error::Error>> {
 
         // transfer 1% of staked amount to the vault token account as interest
         if payer_is_mint_authority == true {
-            let interest = 0.01 * token_account_data.amount as f64;
-
             let payer_associated_token_account =
                 get_associated_token_address(&payer.pubkey(), &mint);
-            let transfer_instruction = transfer(
-                &TOKEN_PROGRAM_ID,
-                &payer_associated_token_account,
-                &vault_token_account_pda,
-                &payer.pubkey(),
-                &[&payer.pubkey()],
-                interest.trunc() as u64,
-            )
-            .unwrap();
 
+            let vault_owner_associated_token_account =
+                get_associated_token_address(&vault_data.owner(), &mint);
+
+            // Build the instruction for send_interest
+            let data = vault::instruction::SendInterest {}.data();
+            let accounts = vault::accounts::Interest {
+                sender: payer.pubkey(),
+                sender_token_account: payer_associated_token_account,
+                owner: *vault_data.owner(),
+                owner_token_account: vault_owner_associated_token_account,
+                mint: *mint,
+                vault: vault_address,
+                vault_authority: vault_authority_pda,
+                vault_token_account: vault_token_account_pda,
+                token_program: TOKEN_PROGRAM_ID,
+            }
+            .to_account_metas(None);
+
+            let send_interest_instruction =
+                Instruction::new_with_bytes(vault_program_id, &data, accounts);
+
+            // Build the transaction
+            let mut transaction =
+                Transaction::new_with_payer(&[send_interest_instruction], Some(&payer.pubkey()));
+
+            // Sign the transaction
             let blockhash = rpc_client.get_latest_blockhash().await?;
+            transaction.sign(&[&payer], blockhash);
 
-            let tx = Transaction::new_signed_with_payer(
-                &[transfer_instruction],
-                Some(&payer.pubkey()),
-                &[&payer],
-                blockhash,
-            );
-            let sig = rpc_client.send_and_confirm_transaction(&tx).await;
+            // Send the transaction to the Solana network
+            let sig = rpc_client
+                .send_and_confirm_transaction(&transaction)
+                .await?;
 
             println!("Interest transfer signature {:?}", sig);
         }
